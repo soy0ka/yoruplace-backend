0adde277439f2aedd4ccd5677d1571da83015446
Yoru <me@rlato.icu>
2022-07-27 16:03:15 +0900

    init

--- src/classes/Database.ts
+++ src/classes/Database.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config'
 import knex, { Knex } from 'knex'
 
 /**
@@ -5,16 +6,15 @@ import knex, { Knex } from 'knex'
 */
 export default class DatabaseClient {
   public db: Knex
-
   constructor () {
     this.db = knex({
       client: 'mysql',
       connection: {
-        host: 'oracle.choabot.com',
-        port: 3306,
-        user: 'choabot',
-        password: 'Yurihana09@',
-        database: 'nextron'
+        host: process.env.DATABASE_HOST || 'localhost',
+        port: Number(process.env.DATABASE_PORT) || 3306,
+        user: process.env.DATABASE_USER,
+        password:  process.env.DATABASE_PASSWORD,
+        database:  process.env.DATABASE_NAME || 'YoruPlace'
       }
     })
   }
